0c525c9c4083a751a979206a13d56a5b042edaf8
JJ Daza <150633974+jcardonadaza@users.noreply.github.com>
2024-06-11 17:07:14 -0500

    init commit

---
+++ JJ Daza.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+
+    //varaibles.
+    int entero = 10; // valores enteros
+    float flotante = 4.5; // La diferencia entre float y double es la cantidad de decimales que puede contener cada uno, es decir que float guarda hasta 7 decimales mientras que double almacena 15 decimales entonces más que todo sería la precisión entre los dos.
+    bool booleanos = true; // evalúa si es verdadero o falso, dependiendo de la condición.
+    bool booleanos2 = false;
+    double doble = 5.6;
+    string cadena = "estoy usando c++"; // es una cadena de caracteres o también una lista.
+    char caracter = 'j'; // es solo un caracter.
+    const float PI = 3.1416; // constante.
+
+    cout << entero << endl; // aquí se está imprimiendo el valor de la variable entero.
+    cout << flotante << endl; // aquí se está imprimiendo el valor de la variable flotante.
+    cout << booleanos << endl;
+    cout << booleanos2 << endl; 
+    cout << doble << endl; // aquí se está imprimiendo el valor de la variable doble.
+    cout << cadena << endl; // aquí se está imprimiendo el valor de la variable cadena.
+    cout << caracter << endl; // aquí se está imprimiendo el valor de la variable caracter.
+    cout << PI << endl; // aquí se está imprimiendo el valor de la constante PI.
+    cout << "Hola mundo en 'c++' " << endl; // aquí se está imprimiendo un mensaje en pantalla o en este caso en la terminal.
+    return 0; // se retorna 0 para indicar que el programa se ejecuto correctamente.
+}
+
+// https://isocpp.org
+
+/*
+hola a todos apenas estoy iniciando
+*/
+
